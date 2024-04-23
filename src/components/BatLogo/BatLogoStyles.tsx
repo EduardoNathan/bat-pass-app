2660edd757b32b0dec0233e530dfcc905ce134d0
Eduardo Nathan <eduardo.nathan2018@outlook.com>
2024-04-23 19:43:25 -0300

    arquivos de design

---
+++ src/components/BatLogo/BatLogoStyles.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  title:{
+    textAlign:'center',
+    fontSize:26,
+    color:'#E5BF3C',
+    fontWeight:'bold'
+  }
+});
+
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import {Text, View} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import styles from './screens/Home/Style'
+import styles from './Style'
+import { BatLogo } from "../../components/BatLogo/BatLogo";
+import { BatTextInput } from "../../components/BatTextInput/BatTextInput";
 
 export default function Home(){
     return(
-        <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
+        <View style={styles.appContainer}>
+
+        <View style={styles.logoContainer}>
+            <BatLogo/>
+        </View>
+
+        <View style={styles.inputContainer}>
+            <BatTextInput/>
+        </View>
+        
         <StatusBar style="auto" />
         </View>
     )
